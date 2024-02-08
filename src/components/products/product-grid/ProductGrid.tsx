9d3b5d622ed7cb11cbfb8ec99160d7fa1fbe1f38
Mario González <mag071@gmail.com>
2024-02-08 12:13:13 -0400

    arreglando menu en mobile 7

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const ProductGrid = ( { products }: Props ) => {
   return (
-    <div className="mx-auto grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
       {
         products.map( product => (
           <ProductGridItem
